a336102505705af3d4cd699a1fe5f31223271ff1
Michael Davies <michaelstoby@gmail.com>
2021-03-04 04:09:45 +0000

    Add implementation of robot api

--- robot.py
+++ robot.py
@@ -1,7 +1,27 @@
+import serial
 
+ser = serial.Serial('/dev/ttyACM0', baudrate=115200)
+print(ser.read(4))
 
 def SetMotors(left, right):
-    pass
+    print(left, right)
+    left *= 128
+    right *= 128
+
+    left += 128
+    right += 128
+
+    if left < 0:
+        left = 0
+    elif left > 255:
+        left = 255
+
+    if right < 0:
+        right = 0
+    elif right > 255:
+        right = 255
+
+    ser.write(bytearray([int(left), int(right)]))
 
 def QueryIrSensor():
-    return 0
+    return 1000
